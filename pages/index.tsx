c8e16333d9e8c86700245089ce6ba0f2005c090a
daku10 <daku.two@gmail.com>
2021-01-03 17:21:04 +0900

    init for the rule of 28

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { Main } from "../components/Main";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>the rule of 28</title>
+      </Head>
+      <Main />
+    </>
+  );
+}
